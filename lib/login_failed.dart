d75f8e68652f04afa173db5f61e02d13d4462495
botrick01 <johannsenpatrick.privat@gmail.com>
2021-12-06 11:21:14 +0100

    Create login_failed.dart

---
+++ lib/login_failed.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.only(top: 50.0),
+      child: Column(
+      children: const [
+        Text('User Login'),
+        TextField(
+          onSubmitted: null,
+          decoration: InputDecoration(
+            labelText: 'Username',
+          )
+        ),
+        TextField(
+            onSubmitted: null,
+            decoration: InputDecoration(
+              labelText: 'Passwort',
+            )
+        ),
+        ElevatedButton(
+            onPressed: null,
+            child: Center(
+              child: Text('Neuer Benutzer'),
+            )
+        ),
+        Text('Username oder Passwort ist falsch'),
+      ]
+    )
+    );
+  }
+}
