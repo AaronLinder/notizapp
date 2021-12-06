1d74685afdf7645c2f8ff4a57d5ea6643be5c0a0
botrick01 <johannsenpatrick.privat@gmail.com>
2021-12-06 10:43:59 +0100

    Update login.dart

--- lib/login.dart
+++ lib/login.dart
@@ -1,1 +1,34 @@
 import 'package:flutter/material.dart';
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+      children: const [
+        Text('User Login'),
+        TextField(
+          onSubmitted: null,
+          decoration: InputDecoration(
+            labelText: 'Username',
+          )
+        ),
+        TextField(
+            onSubmitted: null,
+            decoration: InputDecoration(
+              labelText: 'Passwort',
+            )
+        ),
+        ElevatedButton(
+            onPressed: null,
+            child: Center(
+              child: Text('Neuer Benutzer'),
+            )
+        )
+      ]
+    )
+    );
+  }
+}
