1c0b87e583c049f81309c5c1ac5b64febacd5f39
botrick01 <johannsenpatrick.privat@gmail.com>
2021-12-13 11:19:40 +0100

    Update create_user_fail.dart

---
+++ lib/create_user_fail.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class create_user_fail extends StatelessWidget {
+  const create_user_fail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Padding(
+            padding: const EdgeInsets.only(left: 50, right: 50),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('Neuer Benutzer'),
+                  const TextField(
+                      onSubmitted: null,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Username',
+                      )),
+                  const TextField(
+                      onSubmitted: null,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Passwort',
+                      )),
+                  const TextField(
+                      onSubmitted: null,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Passwort bestätigen',
+                      )),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, "login");
+                      },
+                      child: const Center(
+                        child: Text('abbrechen'),
+                      )),
+                  Text('Dieser User existiert bereits'),
+                ])));
+  }
+}
