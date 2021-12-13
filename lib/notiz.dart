81f3502d87b5f2058337ee4db048d5a5bc5f399c
botrick01 <johannsenpatrick.privat@gmail.com>
2021-12-13 11:39:56 +0100

    Create notiz.dart

---
+++ lib/notiz.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class login extends StatelessWidget {
+  const login({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          alignment: Alignment.center,
+          padding: EdgeInsets.all(50.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                TextField(
+                    onSubmitted: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Title',
+                    )),
+                TextField(
+                    onSubmitted: null,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Notiztext',
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'notizenuebersicht');
+                    },
+                    child: Center(
+                      child: Text('zurück'),
+                    )),
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, 'notizenuebersicht');
+                    },
+                    child: Center(
+                      child: Text('speichern'),
+                    ))
+              ])),
+    );
+  }
+}
