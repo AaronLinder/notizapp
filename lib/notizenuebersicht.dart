65f2ba60106983b3f65c1b66fec1602f4c6213ae
AaronLinder <91245181+AaronLinder@users.noreply.github.com>
2022-01-10 12:08:15 +0100

    Erweiterung der Notizübersicht

--- lib/notizenuebersicht.dart
+++ lib/notizenuebersicht.dart
@@ -5,17 +5,42 @@ class uebersicht extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Container(
+        decoration: new BoxDecoration(color: Colors.white),
+        child: Column(
                 children: [
+                Row(children: [
+        Align(
+        alignment: Alignment.topLeft,
+            child: Container(
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black)
+                ),
+                child:
+                DropdownButton(items: null)
+            )),
+                Expanded(child:
                 Align(
                 alignment: Alignment.topRight,
-                child: ElevatedButton(
+                child: SizedBox(
+                    width: 100, // <-- Your width
+                    height: 50,
+                    child: ElevatedButton(
                       onPressed: () {
                         Navigator.pushNamed(context, "login");
                       },
                       child: const Center(
-                        child: Text('abbrechen'),
+                        child: Text('logout'),
                       ))
-                )]);
+                )))]),
+
+        const Expanded(
+          child: Align(
+              alignment: Alignment.bottomRight,
+              child: ElevatedButton(onPressed: null, child: Icon(Icons.add)),
+          ))]));
+
+
+
   }
 }
