82dbbdd9cff88e622262ed687dc65720ea7aca75
AaronLinder <91245181+AaronLinder@users.noreply.github.com>
2022-01-13 09:17:31 +0100

    fehlerbehebung beim wechsel von Notizen zu Notizübersicht

--- lib/notizenuebersicht.dart
+++ lib/notizenuebersicht.dart
@@ -5,40 +5,41 @@ class uebersicht extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+        body: Container(
         decoration: new BoxDecoration(color: Colors.white),
         child: Column(
-                children: [
-                Row(children: [
+          children: [
+          Row(children: [
         Align(
         alignment: Alignment.topLeft,
-            child: Container(
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black)
-                ),
-                child:
-                DropdownButton(items: null)
-            )),
-                Expanded(child:
-                Align(
-                alignment: Alignment.topRight,
-                child: SizedBox(
-                    width: 100, // <-- Your width
-                    height: 50,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "login");
-                      },
-                      child: const Center(
-                        child: Text('logout'),
-                      ))
-                )))]),
+      child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.black)
+          ),
+          child:
+          DropdownButton(items: null)
+      )),
+          Expanded(child:
+          Align(
+          alignment: Alignment.topRight,
+          child: SizedBox(
+              width: 100, // <-- Your width
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, "login");
+                },
+                child: const Center(
+                  child: Text('logout'),
+                ))
+          )))]),
 
         const Expanded(
-          child: Align(
-              alignment: Alignment.bottomRight,
-              child: ElevatedButton(onPressed: null, child: Icon(Icons.add)),
-          ))]));
+    child: Align(
+        alignment: Alignment.bottomRight,
+        child: ElevatedButton(onPressed: null, child: Icon(Icons.add)),
+    ))])));
 
 
 
