c5c6aa4639a6a3e78244d1d043f57ea1cb500bc8
AaronLinder <91245181+AaronLinder@users.noreply.github.com>
2022-02-21 11:16:15 +0100

    login

--- lib/login.dart
+++ lib/login.dart
@@ -7,33 +7,42 @@ class login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          alignment: Alignment.center,
-          padding: EdgeInsets.all(50.0),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Text('User Login'),
-                TextField(
-                    onSubmitted: null,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'E-mail',
-                    )),
-                TextField(
-                    onSubmitted: null,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Passwort',
-                    )),
-                ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, 'create');
-                    },
-                    child: Center(
-                      child: Text('Neuer Benutzer erstellen'),
-                    ))
-              ])),
+        alignment: Alignment.center,
+        padding: EdgeInsets.all(50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text('User Login'),
+            TextField(
+                onSubmitted: null,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'E-mail',
+                )),
+            TextField(
+              onSubmitted: null,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Passwort',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: ,
+              child: Center(
+                child: Text("login"),
+              ),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, 'create');
+                },
+                child: Center(
+                  child: Text('Neuer Benutzer erstellen'),
+                ))
+          ],
+        ),
+      ),
     );
   }
 }
